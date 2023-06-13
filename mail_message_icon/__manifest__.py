d558c4e988538a3041c81d5d4e99e16b7dc2707a
Stefan W <stefan.wiselka@solvti.pl>
2023-06-13 14:25:19 +0200

    .

--- mail_message_icon/__manifest__.py
+++ mail_message_icon/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Advanced Mail Icons in Chatter",
     "summary": "Additional mail icons, enabling faster identification of email statuses",
-    "version": "13.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Discuss",
     "author": "Solvti Sp. z o.o.",
     "website": "https://www.solvti.pl",
@@ -13,10 +13,18 @@
     "auto_install": False,
     "depends": ["mail", "calendar"],
     "data": [
-        "views/assets.xml",
-        "views/mail_message_view.xml",
+        "views/mail_message_views.xml",
     ],
     "qweb": ["static/src/xml/mail_message_icon.xml"],
+    'assets': {
+        'web.assets_backend': [
+            'mail_message_icon/static/src/components/advance_message_icon/mail_message_icon.js',
+            'mail_message_icon/static/src/components/advance_message_icon/thread_message_icon.xml',
+        ],
+        'web.assets_common': [
+            'mail_message_icon/static/src/components/advance_message_icon/style.css',
+        ],
+    },
     "demo": [],
     "external_dependencies": {},
     "images": ["static/description/main.png"],
